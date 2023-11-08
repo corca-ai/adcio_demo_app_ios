5a6c292dc074de8d9b9545a0d0cc78b297ca3645
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-08 14:50:26 +0900

    feat: add bottom navigation item views

---
+++ adcio_demo_app_ios/Screen/Main/MainScreen.swift
@@ -0,0 +1,21 @@
+//
+//  Main.swift
+//  adcio_demo_app_ios
+//
+//  Created by 유현명 on 11/8/23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Main")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainView()
+}
