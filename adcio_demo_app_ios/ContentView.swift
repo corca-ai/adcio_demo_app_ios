707c90b88e2d30234f08771177bead10e534d2d2
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-08 11:50:41 +0900

    feat: init project

---
+++ adcio_demo_app_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  adcio_demo_app_ios
+//
+//  Created by 유현명 on 11/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ adcio_demo_app_ios/adcio_demo_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  adcio_demo_app_iosApp.swift
+//  adcio_demo_app_ios
+//
+//  Created by 유현명 on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct adcio_demo_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
