042d1a0598217f9b19e4ea8b8af78bc2381675a9
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-08 14:50:04 +0900

    feat: add bottom navigation

--- adcio_demo_app_ios/ContentView.swift
+++ adcio_demo_app_ios/ContentView.swift
@@ -8,14 +8,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .black
+        
+        appearance.stackedLayoutAppearance.selected.iconColor = .white
+        appearance.stackedLayoutAppearance.normal.iconColor = .gray
+        
+        UITabBar.appearance().scrollEdgeAppearance = appearance
+        UITabBar.appearance().standardAppearance = appearance
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            MainView()
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("홈")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        },
+                        icon: {
+                            Image(systemName: "house.fill")
+                                .font(.system(size: 22))
+                        }
+                    )
+                }
+            
+            RecommendView()
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("추천")
+                                .font(.headline) // 글자 크기 조절
+                        },
+                        icon: {
+                            Image(systemName: "star.fill")
+                                .font(.system(size: 22))
+                        }
+                    )
+                    .foregroundStyle(.white)
+                }
+            
+            ChartView()
+                .tabItem {
+                    Label(
+                        title: {
+                            Text("목록")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        },
+                        icon: {
+                            Image(systemName: "list.bullet")
+                                .font(.system(size: 22))
+                        }
+                    )
+                }
         }
-        .padding()
+        .font(.headline)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
