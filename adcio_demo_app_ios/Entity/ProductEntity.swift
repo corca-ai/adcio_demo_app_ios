84f526b9005328803d20d5b449fecd441473f443
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-09 16:21:05 +0900

    feat: add product entity

---
+++ adcio_demo_app_ios/Entity/ProductEntity.swift
@@ -0,0 +1,26 @@
+//
+//  ProductEntity.swift
+//  adcio_demo_app_ios
+//
+//  Created by 유현명 on 11/9/23.
+//
+
+import Foundation
+
+struct ProductEntity: Codable {
+    let id: String
+    let name: String
+    let image: String
+    let price: Int
+    let seller: String
+    let isAd: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case name = "Name (KO)"
+        case image = "Image URL"
+        case price = "Customer Price"
+        case seller = "Winery"
+        case isAd = "Available"
+    }
+}
