5cb526305a74f8852fcbe483cd0c534f9a38c185
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-14 12:25:46 +0900

    feat: update main screen to call product detail view

--- adcio_demo_app_ios/Screen/Main/MainScreen.swift
+++ adcio_demo_app_ios/Screen/Main/MainScreen.swift
@@ -13,49 +13,57 @@ struct MainView: View {
     @State private var products: [ProductEntity] = []
     
     var body: some View {
-        VStack {
-            HStack {
-                Image("corca_logo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 30)
-                    .padding([.top, .leading], 5)
-                Spacer()
-                Image(systemName: "magnifyingglass")
-                    .frame(width: 30, height: 30)
-                    .padding([.top, .trailing], 10)
-            }
-            .padding([.bottom], 5)
-            
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Image("Banner")
+        NavigationView {
+            VStack {
+                HStack {
+                    Image("corca_logo")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
-                    Text("민채님을 위한 추천 상품")
-                        .font(.subheadline)
-                        .padding([.top], 3)
-                        .padding([.leading, .bottom], 15)
-                    ScrollView(.horizontal) {
-                        LazyHGrid(rows: [GridItem(.flexible(minimum: 170), spacing: 8), GridItem(.flexible(minimum: 170), spacing: 8)], spacing: 2) {
-                            ForEach(products, id: \.id) { product in
-                                GridItemView(
-                                    id: product.id,
-                                    name: product.name,
-                                    seller: product.seller,
-                                    price: product.price,
-                                    image: product.image,
-                                    isAd: product.isAd
-                                )
+                        .frame(width: 100, height: 30)
+                        .padding([.top, .leading], 5)
+                    Spacer()
+                    Image(systemName: "magnifyingglass")
+                        .frame(width: 30, height: 30)
+                        .padding([.top, .trailing], 10)
+                }
+                .padding([.bottom], 5)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 15) {
+                        Image("Banner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                        
+                        Text("민채님을 위한 추천 상품")
+                            .font(.subheadline)
+                            .padding([.top], 3)
+                            .padding([.leading, .bottom], 15)
+                        
+                        ScrollView(.horizontal) {
+                            LazyHGrid(rows: [GridItem(.flexible(minimum: 170), spacing: 8), GridItem(.flexible(minimum: 170), spacing: 8)], spacing: 2) {
+                                ForEach(products, id: \.id) { product in
+                                    let productValue = ProductEntity(
+                                        id: product.id,
+                                        name: product.name,
+                                        image: product.image,
+                                        price: product.price,
+                                        seller: product.seller,
+                                        isAd: product.isAd
+                                    )
+                                    NavigationLink(destination: ProductDetailView(productValue: productValue)) {
+                                        GridItemView(productValue: productValue)
+                                            .frame(width: 150)
+                                    }
+                                }
                             }
-                            .frame(width: 150) // Adjust the width as needed
                         }
                     }
                 }
             }
-        }
-        .onAppear {
-            products = fetchJsonData() + fetchSuggestData()
+            .onAppear {
+                products = fetchJsonData() + fetchSuggestData()
+            }
+            .navigationBarTitle("", displayMode: .inline)
         }
     }
 }
