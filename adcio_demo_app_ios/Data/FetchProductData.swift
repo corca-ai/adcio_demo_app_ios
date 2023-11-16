a6f274c2104c6c0830e984b5cf598fc370be8add
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-16 14:23:13 +0900

    refactor: separate fetch suggestion & logOption data value method

--- adcio_demo_app_ios/Data/FetchProductData.swift
+++ adcio_demo_app_ios/Data/FetchProductData.swift
@@ -28,25 +28,54 @@ func fetchJsonData() -> [ProductEntity] {
     return productValue
 }
 
-func fetchSuggestData(completion: @escaping ([AdcioSuggestion]) -> Void) {
-    
-    var suggestValue: [AdcioSuggestion] = []
-    
+func fetchSuggestData(
+    suggestion: @escaping ([ProductEntity]) -> Void,
+    logOption: (([LogOptionEntity]) -> Void)? = nil
+) {
+
+    var productValue: [ProductEntity] = []
+    var logOptionValue: [LogOptionEntity] = []
+
     try? AdcioPlacement.shared.adcioCreateSuggestion(
-        placementId: "67592c00-a230-4c31-902e-82ae4fe71866",
-        onSuccess: { adcioSuggestionRawData in
-            let suggestionData = adcioSuggestionRawData.suggestions
-            for index in 0..<suggestionData.count {
-                suggestValue.append(suggestionData[index])
-            }
-            completion(suggestValue)
-        },
-        onFailure: { error in
-            dump("Placement call is failed")
-            completion([])
+        placementId: "67592c00-a230-4c31-902e-82ae4fe71866"
+    ) { AdcioSuggestionRawData in
+        let suggestionData = AdcioSuggestionRawData.suggestions
+
+        for index in 0..<suggestionData.count {
+            let suggestedProduct = ProductEntity(
+                id: suggestionData[index].product.id,
+                name: suggestionData[index].product.name,
+                image: suggestionData[index].product.image,
+                price: suggestionData[index].product.price,
+                seller: suggestionData[index].product.description,
+                isAd: true
+            )
+            productValue.append(suggestedProduct)
         }
-    )
+        suggestion(productValue)
+
+        if let logOption = logOption {
+            for index in 0..<suggestionData.count {
+                let logOptions = LogOptionEntity(
+                    requestId: suggestionData[index].logOptions.requestId,
+                    adsetId: suggestionData[index].logOptions.adsetId
+                )
+                logOptionValue.append(logOptions)
+            }
+            logOption(logOptionValue)
+        }
+    } onFailure: { Error in
+        // Handle failure
+        @State var showToast = true
+        ToastView(isVisible: $showToast, hideAfter: 2) {
+            Text("Placement call is failed")
+                .padding()
+                .background(Color.black)
+                .foregroundColor(Color.white)
+                .cornerRadius(10)
+        }
+        dump("Placement call is failed")
+        suggestion([])
+        logOption?([]) // Use optional chaining to call logOption if it's not nil
+    }
 }
-
-
-
