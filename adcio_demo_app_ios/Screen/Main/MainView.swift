c80ddb84662c064b7562cff25bd3c662c9d97ea2
yoohyunmyeong <aahspringaa4@naver.com>
2023-11-17 14:58:11 +0900

    feat: add talk icon to move adcio agent

--- adcio_demo_app_ios/Screen/Main/MainView.swift
+++ adcio_demo_app_ios/Screen/Main/MainView.swift
@@ -14,56 +14,72 @@ struct MainView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                HStack {
-                    Image("corca_logo")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 30)
-                        .padding([.top, .leading], 5)
-                    Spacer()
-                    Image(systemName: "magnifyingglass")
-                        .frame(width: 30, height: 30)
-                        .padding([.top, .trailing], 10)
-                }
-                .padding([.bottom], 5)
-                
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 15) {
-                        Image("Banner")
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
+                VStack {
+                    HStack {
+                        Image("corca_logo")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                        
-                        Text("민채님을 위한 추천 상품")
-                            .font(.subheadline)
-                            .padding([.top], 3)
-                            .padding([.leading, .bottom], 15)
-                        
-                        ScrollView(.horizontal) {
-                            LazyHGrid(rows: [GridItem(.flexible(minimum: 170), spacing: 8), GridItem(.flexible(minimum: 170), spacing: 8)], spacing: 2) {
-                                ForEach(products, id: \.id) { product in
-                                    let productValue = ProductEntity(
-                                        id: product.id,
-                                        name: product.name,
-                                        image: product.image,
-                                        price: product.price,
-                                        seller: product.seller,
-                                        isAd: product.isAd
-                                    )
-                                    NavigationLink(destination: ProductDetailView(productValue: productValue)) {
-                                        GridItemView(productValue: productValue)
-                                            .frame(width: 150)
+                            .frame(width: 100, height: 30)
+                            .padding([.top, .leading], 5)
+                        Spacer()
+                        Image(systemName: "magnifyingglass")
+                            .frame(width: 30, height: 30)
+                            .padding([.top, .trailing], 10)
+                    }
+                    .padding([.bottom], 5)
+                    
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 15) {
+                            Image("Banner")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                            
+                            Text("민채님을 위한 추천 상품")
+                                .font(.subheadline)
+                                .padding([.top], 3)
+                                .padding([.leading, .bottom], 15)
+                            
+                            ScrollView(.horizontal) {
+                                LazyHGrid(rows: [GridItem(.flexible(minimum: 170), spacing: 8), GridItem(.flexible(minimum: 170), spacing: 8)], spacing: 2) {
+                                    ForEach(products, id: \.id) { product in
+                                        let productValue = ProductEntity(
+                                            id: product.id,
+                                            name: product.name,
+                                            image: product.image,
+                                            price: product.price,
+                                            seller: product.seller,
+                                            isAd: product.isAd
+                                        )
+                                        NavigationLink(destination: ProductDetailView(productValue: productValue)) {
+                                            GridItemView(productValue: productValue)
+                                                .frame(width: 150)
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
+                ZStack {
+                    NavigationLink(destination: ChatBotView()) {
+                        Circle()
+                            .foregroundColor(Color(UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0)))
+                            .frame(width: 50, height: 50)
+                            .overlay(
+                                Image("agent_talk")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 32, height: 32)
+                                    .padding(.bottom, 2)
+                            )
+                            .padding([.trailing, .bottom], 20)
+                    }
+                }
+                .onAppear {
+                    products = fetchJsonData() + fetchSuggestData()
+                }
             }
-            .onAppear {
-                products = fetchJsonData() + fetchSuggestData()
-            }
-            .navigationBarTitle("", displayMode: .inline)
         }
     }
 }
